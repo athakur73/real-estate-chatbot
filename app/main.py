af42757768e9fabbf669d118d70c20c0ea32ea8f
Amit Thakur <43364783+athakur73@users.noreply.github.com>
2025-12-29 16:40:02 +0530

    Update CORS allowed origins to include production URL

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app = FastAPI(
 # ✅ CORS CONFIG (IMPORTANT)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5185","http://localhost:5186", "http://localhost:3000"],
+    allow_origins=["http://localhost:5185","http://localhost:5186", "https://real-estate-chatbot-t11h.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
